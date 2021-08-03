b97dd502756a9df7ae08fdd70f50c0819d6475b9
pranav157 <pranavgupta.v@gmail.com>
2021-08-03 16:00:20 +0200

    updated features and fixed some bugs

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const open = document.getElementById("open");
 const close = document.getElementById("close");
 const container = document.querySelector(".container");
 
-open.addEventlistener("click", () => container.classList.add("show-nav"));
-close.addEventlistener("click", () => container.classList.remove("show-nav"));
+open.addEventListener("click", () => container.classList.add("show-nav"));
+
+close.addEventListener("click", () => container.classList.remove("show-nav"));
